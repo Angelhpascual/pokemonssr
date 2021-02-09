7e1f929a346b2f8b4aee9d2b2bbdc7b7e67c0f80
angelhpascual <angelhpascual@gmail.com>
2021-02-09 19:57:31 +0100

    SSR TEST

---
+++ pages/pokemon.js
@@ -0,0 +1,53 @@
+import Layout from "../components/Layout";
+import Link from "next/link";
+
+const Pokemon = ({ pokeman }) => {
+  console.log(pokeman);
+  return (
+    <Layout title={pokeman.name}>
+      <h1 className="text-2xl capitalize text-center">{pokeman.name}</h1>
+      <img
+        className="mx-auto w-64 h-64"
+        src={pokeman.image}
+        alt={pokeman.name}
+      />
+      <div className="text-center">
+        <p>
+          <span className="font-bold mr-2">Weight:</span>
+          {pokeman.weight} kg
+        </p>
+        <p>
+          <span className="font-bold mr-2">Height:</span>
+          {pokeman.height} m
+        </p>
+        <h2 className="text-2xl mb-2 mt-4">Abilities:</h2>
+        {pokeman.abilities.map((abi, index) => (
+          <p key={index}>{abi.ability.name}</p>
+        ))}
+        <h2 className="text-2xl mb-2 mt-4">Types:</h2>
+        {pokeman.types.map((type, index) => (
+          <p key={index}>{type.type.name}</p>
+        ))}
+      </div>
+    </Layout>
+  );
+};
+export async function getServerSideProps({ query }) {
+  const id = query.id;
+  try {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const pokeman = await res.json();
+    const paddedIndex = ("00" + id).slice(-3);
+    const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${paddedIndex}.png`;
+    pokeman.image = image;
+    return {
+      props: {
+        pokeman,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+export default Pokemon;
